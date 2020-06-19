f33a0a76232f2f73e8d9c59a96ffce4015d4ea46
YYY <1004473299@qq.com>
2020-06-19 20:31:26 +0800

    更新了检测录音频率的算法。现在比较准确了

--- utils/波处理.py
+++ utils/波处理.py
@@ -14,6 +14,7 @@ def 读取wav文件(文件名):
 
 def 时域转频域(x , f):
 	'''输入一个波形，输出各个频率分量'''
+
 	n = len(x)
 	y = np.fft.fft(x , n)
 	y = abs(y)
@@ -21,19 +22,68 @@ def 时域转频域(x , f):
 	y = y[:n//2] #只取前一半
 	x = (f * np.array(range(n)) / n)[:n//2]
 
-	# 在0附近常常有不正常的波峰，我也不知道为啥
-	for i in range(50 * 5):
+	#在100以内有噪音
+	for i in range(1000):
+		if x[i] > 100:break
 		y[i] = 0
 
 	return y , x #纵坐标，横坐标
 
+def 平滑(y , k = 10):
+	for i in range(k):
+		for i in range(1 , len(y)-1):
+			y[i] = (y[i-1] + y[i] + y[i+1]) / 3
+	return y
+
+
+def 寻找波峰(y):
+	ret = []
+	for i in range(1 , len(y)-1):
+		if (y[i] > y[i-1]) and (y[i] > y[i+1]):
+			if len(ret) > 0 and abs(i - ret[-1]) < 10: #不要太近
+				continue
+			ret.append(i)
+	return ret 
+
+
+
 def 求主导频率(x , f):
 	'''输入一段复合波形，输出主导分量的频率'''
 
+	x = 平滑(x , 10)
 	y , x = 时域转频域(x,f)
+	y = 平滑(y , 10)
 
-	best_y = np.argmax(y)
-	best_x = x[best_y]
+	波峰 = 寻找波峰(y)
+	最高峰 = np.max(y[波峰])
+
+	pos = 波峰[0]
+	for p in 波峰:
+		if y[p] > 最高峰/4: #至少是最高峰的1/4
+			pos = p
+			break
+
+	best_x = x[pos]
 
 	return best_x
 
+def 画频率分布图(x , f , noise = (None,None) , lim = -1):
+
+	nx , nf = noise
+
+	x = 平滑(x , 10)
+	y , x = 时域转频域(x,f)
+
+	if nx is not None:
+		nx = 平滑(nx , 10)
+		ny , nx = 时域转频域(nx,nf)
+		y -= ny
+
+	y = 平滑(y)
+
+	if lim > 0:
+		t = 1 / (x[1] - x[0])
+		x = x[:int(lim * t)]
+		y = y[:int(lim * t)]
+	plt.plot(x , y)
+	plt.show()
--- 自由练习_唱.py
+++ 自由练习_唱.py
@@ -1,10 +1,11 @@
 from modules.频率记号对应表 import 获得记号编号 , 获得记号区间 , 去除黑键 , 从记号获得频率 , 获得最接近记号
 import random
-from modules.检测录音频率 import 检测录音频率 
+from modules.检测录音频率 import 检测录音频率 , 获取录音
 from modules.播放随机波 import 播放随机波 
-from utils.波处理 import 读取wav文件 , 时域转频域 , 求主导频率
+from utils.波处理 import 读取wav文件 , 时域转频域 , 求主导频率 , 画频率分布图
 import copy
 import os
+import time
 
 def 自由练习_唱():
 	os.system("cls")
@@ -14,13 +15,21 @@ def 自由练习_唱():
 			"2)退出\n" + 
 			">> "
 		).strip()
-
+		if x == "":
+			x = 1
 		if int(x) == 2:
 			break
 
-		w = 检测录音频率(2)
+		x , f = 获取录音(1)
+
+		#画频率分布图(x , f , lim = 800)
+
+		w = 求主导频率(x , f)
 		print ("你唱的频率是：%.2fHz" % w)
+		
 		m , diff = 获得最接近记号(w)
 		print ("最接近的琴键是：%s (%.2fHz)，差距是%.2fHz" % (m , 从记号获得频率(m) , diff))
+
+
 
 		print ("------------------------------------------------")
